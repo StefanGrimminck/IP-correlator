c9e88b99361751bf6f5dd596e830f3309c8f976c
Stefan <s.j.grimminck@gmail.com>
2019-09-29 15:17:21 +0200

    Create main.go
    initial commit

---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+type host struct {
+	IPv4      string
+	IPv6      string
+	hostname  string
+	IPv4Ports []int
+	IPV6Ports []int
+	next      *host
+}
+
+func main() {
+	al := genHostList("NL_IPv6.csv")
+
+	for p := al; p.next != nil; p = p.next {
+		fmt.Printf("IPv4: %v, IPv6: %v, Hostname: %v\n", p.IPv4, p.IPv6, p.hostname)
+	}
+}
+
+func enrich(i *host) bool {
+	if addr := net.ParseIP(i.IPv6); addr == nil {
+		return false
+	}
+
+	if i.hostname == "" {
+		record, err := net.LookupAddr(i.IPv6)
+		if err != nil || len(record) == 0 {
+			return false
+		}
+		i.hostname = record[0]
+	}
+
+	//Get IPv4 from hostname
+	IP, err := net.LookupHost(i.hostname)
+
+	if err != nil {
+		return false
+	}
+
+	for _, a := range (IP) {
+		netA := net.ParseIP(a)
+
+		if netA.To4() != nil {
+			i.IPv4 = netA.String()
+			return true
+		}
+	}
+	return false
+}
+
+func genHostList(fileName string) *host {
+	var hostList host
+	csvFile, err := os.Open(fileName)
+
+	if err != nil {
+		fmt.Println("Error, could not open input file: ", err)
+	}
+
+	r := csv.NewReader(bufio.NewReader(csvFile))
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		temp := host{
+			IPv6:     record[0],
+			hostname: record[4],
+		}
+
+		if OK := enrich(&temp); OK {
+			addNode(&temp, &hostList)
+		}
+	}
+
+	if hostList.IPv4 == "" {
+		hostList = *hostList.next
+	}
+
+	return &hostList
+}
+
+func addNode(newHost, hostList *host) *host {
+	if hostList == nil {
+		return newHost
+	}
+
+	for p := hostList; p != nil; p = p.next {
+		if p.next == nil {
+			p.next = newHost
+			return hostList
+		}
+	}
+	return hostList
+}
